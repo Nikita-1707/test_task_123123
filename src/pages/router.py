4f781ba5574f8b26214d32146a9c71a01d970117
nikitasamvl <nikita.samoilov@improvado.io>
2024-03-08 19:33:43 +0500

    add login page

--- src/pages/router.py
+++ src/pages/router.py
@@ -16,3 +16,8 @@ def get_home_page(request: Request):
 @router.get('/registration')
 def get_registration_page(request: Request):
     return templates.TemplateResponse('registration.html', {'request': request})
+
+
+@router.get('/login')
+def get_login_page(request: Request):
+    return templates.TemplateResponse('authorization.html', {'request': request})
